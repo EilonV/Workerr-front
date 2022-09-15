871f3f161d06f68ea339e8b2fd933c644f2c57bf
EilonV <73593619+EilonV@users.noreply.github.com>
2022-09-15 17:04:56 +0300

    added form hooks, trying crud in explore

--- src/views/gig-explore.jsx
+++ src/views/gig-explore.jsx
@@ -1,10 +1,22 @@
 import { useEffect, useState } from "react"
-import { useSelector } from "react-redux"
+import { useSelector, useDispatch } from "react-redux"
 import { gigService } from "../services/gig.service"
+import { loadGigs } from "../store/actions/gig.action"
 
 export const GigExplore = () => {
 
-    return (
+    const gigs = useSelector(state => state.gigModule.gigs)
+    const dispatch = useDispatch()
+
+    useEffect(() => {
+        dispatch(loadGigs())
+    }, [])
+
+    return (<section>
         <h2>EXPLORE</h2>
+        <div>
+            
+        </div>
+    </section>
     )
 } 
