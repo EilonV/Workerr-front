67e82249f192bdba7c7fcfd0a0971c4614ea5acb
EilonV <73593619+EilonV@users.noreply.github.com>
2022-09-15 14:46:43 +0300

    added local gig to storage

--- src/services/gig.service.js
+++ src/services/gig.service.js
@@ -1,23 +1,83 @@
-import { storageService } from './async-storage.service.js'
+import { storageService } from './async-storage'
 import { utilService } from './util.service.js'
 import { userService } from './user.service.js'
+import { store } from '../store/store'
 import {
   getActionRemoveGig,
   getActionAddGig,
   getActionUpdateGig,
-} from '../store/gig.actions.js'
-import { store } from '../store/store'
+} from '../store/actions/gig.action'
+
+const gDefaultGigs = [{
+  _id: i101,
+  title: 'I will design your logo',
+  price: 12,
+  owner: {
+    _id: u101,
+    fullname: 'Dudu Da',
+    imgUrl: url,
+    level: basic / premium,
+    rate: 4
+  },
+  daysToMake: 3,
+  description: 'Make unique logo...',
+  imgUrl: '',
+  tags: [
+    logo - design,
+    artisitic,
+    proffesional,
+    accessible
+  ],
+  likedByUsers: ['mini-user'], // for user-wishlist : use $in
+  order: [
+    {
+      _id: 'o1225',
+      createdAt: 9898989,
+      buyer: mini - user,
+      seller: mini - user,
+      gig: {
+        _id: i101,
+        name: 'Design Logo',
+        price: 20
+      },
+      status: pending
+    }
+  ],
+  user: [
+    {
+      _id: u101,
+      fullname: 'User 1',
+      imgUrl: url,
+      username: 'user1',
+      password: 'secret',
+      level: basic / premium,
+      reviews: [
+        {
+          id: madeId,
+          txt: 'Very kind and works fast',
+          rate: 4,
+          by: {
+            _id: u102,
+            fullname: user2,
+            imgUrl: /img/img2.jpg
+          }
+        }
+      ],
+    },
+  ]
+}
+]
 
 // This file demonstrates how to use a BroadcastChannel to notify other browser tabs
 
 const STORAGE_KEY = 'gig'
 const gigChannel = new BroadcastChannel('gigChannel')
 
-;(() => {
-  gigChannel.addEventListener('message', (ev) => {
-    store.dispatch(ev.data)
-  })
-})()
+  ; (() => {
+    gigChannel.addEventListener('message', (ev) => {
+      store.dispatch(ev.data)
+    })
+  })()
 
 export const gigService = {
   query,
