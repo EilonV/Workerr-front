293c8119317aa47bfb02c42888ced5b667ef1141
EilonV <73593619+EilonV@users.noreply.github.com>
2022-09-15 19:15:17 +0300

    updated link detail in preview

--- src/cmps/gig-preview.jsx
+++ src/cmps/gig-preview.jsx
@@ -3,7 +3,7 @@ import { Link, useNavigate, useParams } from 'react-router-dom'
 export const GigPreview = ({ gig }) => {
 
     return <section className="gig-preview">
-        <Link to={`/gig/edit/${gig._id}`} >
+        <Link to={`/gig/details/${gig._id}`} >
             <h1>GIG PREVIEW</h1>
             <p>{gig.title}</p>
             <p>{gig.price}</p>
