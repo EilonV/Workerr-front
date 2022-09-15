3dea142cccb936422ee9e23431ebdf5629f7bfa9
Ilya Griss <ilyaig8@gmail.com>
2022-09-16 01:18:59 +0300

    root-cmp commit

--- src/root-cmp.jsx
+++ src/root-cmp.jsx
@@ -8,37 +8,15 @@ import { GigExplore } from './views/gig-explore'
 import { UserPage } from './views/user-page'
 import { AdminPage } from './views/admin-page'
 import { GigEdit } from './views/gig-edit'
-import { GigDetails } from './views/gig-details';
+import { GigDetails } from './views/gig-details'
 
 function App() {
-<<<<<<< HEAD
-
-
-    return (
-        <div className="main-app main-layout">
-            <AppHeader />
-            <main className='main-container'>
-                <Routes>
-                    <Route path='gig/details/:id' element={<GigDetails />} />
-                    <Route path='gig/edit/:id' element={<GigEdit />} />
-                    <Route path='gig/edit' element={<GigEdit />} />
-                    <Route path='/about' element={<About />} />
-                    <Route path='/gigs' element={<GigExplore />} />
-                    <Route path='/user' element={<UserPage />} />
-                    <Route path='/admin' element={<AdminPage />} />
-                    <Route path='/' element={<Home />} />
-                </Routes>
-
-            </main>
-            <AppFooter />
-        </div>
-    )
-=======
   return (
     <div className='main-app main-layout'>
       <AppHeader />
       <main className='main-container'>
         <Routes>
+          <Route path='gig/details/:id' element={<GigDetails />} />
           <Route path='gig/edit/:id' element={<GigEdit />} />
           <Route path='gig/edit' element={<GigEdit />} />
           <Route path='/about' element={<About />} />
@@ -51,7 +29,5 @@ function App() {
       <AppFooter />
     </div>
   )
->>>>>>> 0bea6b7fb757f6bdcfea9e46a523cce8af413a82
 }
-
 export default App
