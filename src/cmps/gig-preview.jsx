1e9a0315f2c7310f128b8962649c00e4ad2ea82a
Ilya Griss <ilyaig8@gmail.com>
2022-09-15 19:22:58 +0300

    1

--- src/cmps/gig-preview.jsx
+++ src/cmps/gig-preview.jsx
@@ -1,14 +1,15 @@
 import { Link, useNavigate, useParams } from 'react-router-dom'
 
 export const GigPreview = ({ gig }) => {
-
-    return <section className="gig-preview">
-        <Link to={`/gig/details/${gig._id}`} >
-            <h1>GIG PREVIEW</h1>
-            <p>{gig.title}</p>
-            <p>{gig.price}</p>
-            <p>{gig.description}</p>
-            <p>created by: {gig.owner.fullname}</p>
-        </Link>
+  return (
+    <section className='gig-preview'>
+      <Link to={`/gig/details/${gig._id}`}>
+        <h1>GIG PREVIEW</h1>
+        <p>{gig.title}</p>
+        <p>{gig.price}</p>
+        <p>{gig.description}</p>
+        <p>created by: {gig.owner.fullname}</p>
+      </Link>
     </section>
+  )
 }
