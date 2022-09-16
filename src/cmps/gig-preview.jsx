f9cbecd8fcff24027b0ddb8f851dad99a2d47c31
EilonV <73593619+EilonV@users.noreply.github.com>
2022-09-16 13:49:06 +0300

    added responsive gig grid

--- src/cmps/gig-preview.jsx
+++ src/cmps/gig-preview.jsx
@@ -2,15 +2,17 @@ import { Link, useNavigate, useParams } from 'react-router-dom'
 
 export const GigPreview = ({ gig }) => {
 
-    return (<section className='gig-preview'>
-        <Link to={`/gig/edit/${gig._id}`} >
-            <h1>GIG PREVIEW</h1>
-            <p>{gig.title}</p>
-            <p>{gig.price}</p>
-            <p>{gig.description}</p>
-            <p>{gig.url}</p>
-            {gig.owner && <p>created by: {gig.owner.fullname}</p>}
-        </Link>
+    return (<section className='gig-preview-wrapper'>
+        <div className='gig-preview'>
+            <Link to={`/gig/edit/${gig._id}`} >
+                <p>{gig.url}</p>
+                <p>{gig.title}</p>
+                <p>{gig.price}</p>
+                <p>{gig.description}</p>
+                {gig.owner && <p>created by: {gig.owner.fullname}</p>}
+            </Link>
+        </div>
+
     </section>
     )
 }
