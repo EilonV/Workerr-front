a70930b0ac05907b4707c18ac20ebf228c00c15a
Ilya Griss <ilyaig8@gmail.com>
2022-09-16 11:49:36 +0300

    preview commit

--- src/cmps/gig-preview.jsx
+++ src/cmps/gig-preview.jsx
@@ -5,7 +5,7 @@ export const GigPreview = ({ gig }) => {
     <section className='gig-preview'>
       <Link to={`/gig/edit/${gig._id}`}>
         <img className='img-gig' src={gig.owner.imgUrl} />
-        <img className='img-user' src={gig.imgUrl} />
+        {/* <img className='img-user' src={gig.imgUrl} /> */}
         {gig.owner && <p> {gig.owner.fullname}</p>}
         <p>{gig.title}</p>
         <p>{`$` + gig.price}</p>
