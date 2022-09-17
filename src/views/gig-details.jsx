d6f3a92180ba4f7f029935530789f8a16fc40350
guy yohpaz <guyos3532@gmail.com>
2022-09-17 11:53:13 +0300

    details-design

--- src/views/gig-details.jsx
+++ src/views/gig-details.jsx
@@ -21,35 +21,45 @@ export const GigDetails = () => {
     console.log(gig)
 
     if (gig) return (
-        <div className='toy-details'>
+        <div className='gig-details'>
 
             <section>
                 <h1>{gig.title}</h1>
 
                 <div className='seller-avatar flex row align-center gap'>
-                <Link to={'/#'} >{gig.owner.fullname}</Link>
+                <img className='seller-img' src="https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8cHJvZmlsZXxlbnwwfHwwfHw%3D&w=1000&q=80" />
+                    <Link to={'/#'} >{gig.owner.fullname}</Link>
                     <h4>{gig.owner.level}</h4>
                     <h4>{gig.owner.rate}</h4>
                 </div>
 
-                <img src={gig.imgUrl} alt='Some Logo Design' />
+                <img className='gig-picture' src={gig.imgUrl} alt='Some Logo Design' />
+
                 <h2>About Gig</h2>
                 <p>{gig.longerDescription} </p>
             </section>
 
-            <h2>About The Seller</h2>
-            <section className='seller-avatar flex row align-center gap'>
-                <img className='img-seller border-radius' src="https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8cHJvZmlsZXxlbnwwfHwwfHw%3D&w=1000&q=80" />
-                <Link to={'/#'} > {gig.owner.fullname}</Link>
-                <p className='seller-level'>{gig.owner.level}</p>
+            <section className='flex column gap'>
+                <h2>About The Seller</h2>
+                <div className='seller-avatar flex row align-center gap'><img className='seller-img' src="https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8cHJvZmlsZXxlbnwwfHwwfHw%3D&w=1000&q=80" />
+                 <div className='flex column'>  <Link to={'/#'} > {gig.owner.fullname}</Link>
+                    <p className='seller-level'>{gig.owner.level}</p>
+                    <p>{gig.owner.rate}</p>
+                    </div> 
+                </div>
+
             </section>
 
             <section className='seller-desc flex column'>
-                <div className='formal-desc flex row'>
-                    <h4> From {gig.owner.ownerCountry}</h4>
-                    <h4> Member Since {gig.owner.memberSince}</h4>
-                    <h4>Avg. response time {gig.owner.avgResponseTime}</h4>
-                    <h4>Last delivery {gig.owner.lastDelivery}</h4>
+                <div className='formal-desc flex row gap'>
+                    <div className='flex column gap'>
+                        <h4><p>From </p>{gig.owner.ownerCountry}</h4>
+                        <h4><p>Member Since</p>{gig.owner.memberSince}</h4>
+                    </div>
+                    <div className='flex column gap'>
+                        <h4><p>Avg. response</p>time {gig.owner.avgResponseTime}</h4>
+                        <h4><p>Last delivery</p>{gig.owner.lastDelivery}</h4>
+                    </div>
                 </div>
 
                 <div className='seller-letter'>
