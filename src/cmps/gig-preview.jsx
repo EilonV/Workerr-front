2cda74c350e1b39607d60258bebedb4bf9c88c5a
EilonV <73593619+EilonV@users.noreply.github.com>
2022-09-17 18:36:00 +0300

    updated footer, home header

--- src/cmps/gig-preview.jsx
+++ src/cmps/gig-preview.jsx
@@ -1,7 +1,6 @@
 import { Link, useNavigate, useParams } from 'react-router-dom'
 
 export const GigPreview = ({ gig }) => {
-    console.log(gig)
     return (
         <section className='gig-preview'>
             <Link to={`/gig/details/${gig._id}`}>
@@ -23,11 +22,10 @@ export const GigPreview = ({ gig }) => {
             </Link>
 
             <div className='gig-reviews-card flex'>
-                <svg className='gig-review-star' xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1792 1792" width="16" height="16"><path fill="#ffbe5b" d="M1728 647q0 22-26 48l-363 354 86 500q1 7 1 20 0 21-10.5 35.5t-30.5 14.5q-19 0-40-12l-449-236-449 236q-22 12-40 12-21 0-31.5-14.5t-10.5-35.5q0-6 2-20l86-500-364-354q-25-27-25-48 0-37 56-46l502-73 225-455q19-41 49-41t49 41l225 455 502 73q56 9 56 46z" /></svg>
-                <p>&nbsp;{gig.owner.rate}</p>
+                <svg className='gig-review-star' xmlns='http://www.w3.org/2000/svg' viewBox='0 0 1792 1792' width='16' height='16'><path fill='#ffbe5b' d='M1728 647q0 22-26 48l-363 354 86 500q1 7 1 20 0 21-10.5 35.5t-30.5 14.5q-19 0-40-12l-449-236-449 236q-22 12-40 12-21 0-31.5-14.5t-10.5-35.5q0-6 2-20l86-500-364-354q-25-27-25-48 0-37 56-46l502-73 225-455q19-41 49-41t49 41l225 455 502 73q56 9 56 46z' /></svg>
+                {gig.owner.rate ? <p>&nbsp;{gig.owner.rate}</p> : <p>&nbsp;0</p>}
                 <div className='flex'>
-                    <p className='user-reviews-count'>({gig.user[0].reviews.length})</p>
-                </div>
+                    {gig.user[0].reviews ? (<p className='user-reviews-count'>({gig.user[0].reviews.length})</p>) : (<p className='user-reviews-count'>(0)</p>)}</div>
             </div>
 
             <div className='gig-footer flex space-between align-center'>
