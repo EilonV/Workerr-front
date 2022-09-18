2fa09b3232aa971e91b73e918371179056522131
Ilya Griss <ilyaig8@gmail.com>
2022-09-18 12:51:21 +0300

    gig-list

--- src/cmps/gig-list.jsx
+++ src/cmps/gig-list.jsx
@@ -2,14 +2,15 @@ import { GigPreview } from './gig-preview'
 
 export const GigList = ({ gigs }) => {
   if (!gigs) return <div className='flex justify-center'>Loading...</div>
-  if (gigs.length > 0) console.log(gigs)
-  return (
-    <section>
-      <div className='gig-list'>
-        {gigs.map((gig) => (
-          <GigPreview key={gig._id} gig={gig} />
-        ))}
-      </div>
-    </section>
-  )
+  if (gigs.length > 0)
+    //  console.log(gigs)
+    return (
+      <section>
+        <div className='gig-list'>
+          {gigs.map((gig) => (
+            <GigPreview key={gig._id} gig={gig} />
+          ))}
+        </div>
+      </section>
+    )
 }
