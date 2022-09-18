6d01da3affc5263ffb1662fddd471f16e68202fe
EilonV <73593619+EilonV@users.noreply.github.com>
2022-09-18 12:51:29 +0300

    removed loop log

--- src/cmps/gig-list.jsx
+++ src/cmps/gig-list.jsx
@@ -2,7 +2,6 @@ import { GigPreview } from './gig-preview'
 
 export const GigList = ({ gigs }) => {
   if (!gigs) return <div className='flex justify-center'>Loading...</div>
-  console.log(gigs)
   return (
     <section>
       <div className='gig-list'>
