22d9e1c6dd2854d7067170153060ef38f92cd439
guy yohpaz <guyos3532@gmail.com>
2022-09-18 21:21:43 +0300

    details

--- src/views/gig-details.jsx
+++ src/views/gig-details.jsx
@@ -25,83 +25,112 @@ export const GigDetails = () => {
 
   if (gig)
     return (
-      <section className='gig-details'>
+      <section className='gig-details main-layout'>
         <AppHeaderExplore />
-        <div className='details-container'>
-          <section>
-            <h1>{gig.title}</h1>
+        <div className='details-container flex row'>
+          <div className='main-details gap'>
+            <section>
+              <h1>{gig.title}</h1>
 
-            <div className='seller-avatar flex row align-center gap'>
-              <img src={gig.owner.imgUrl} alt='' />
+              <div className='seller-avatar flex row align-center gap'>
+                <img className='gig-owner-image-top border-radius' src={gig.owner.imgUrl} alt='' />
 
-              {/* <img
+                {/* <img
                 className='gig-owner-image border-radius'
                 src='https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8cHJvZmlsZXxlbnwwfHwwfHw%3D&w=1000&q=80'
               /> */}
-              <Link to={'/#'}>{gig.owner.fullname}</Link>
-              <h4>{gig.owner.level}</h4>
-              <h4>{gig.owner.rate}</h4>
-            </div>
+                <Link to={'/#'}>{gig.owner.fullname}|</Link>
+                <h4>{gig.owner.level}Link|</h4>
+                <h4>{gig.owner.rate}</h4>
+                <svg
+                  className='gig-review-star'
+                  xmlns='http://www.w3.org/2000/svg'
+                  viewBox='0 0 1792 1792'
+                  width='16'
+                  height='16'
+                >
+                  <path
+                    fill='#ffbe5b'
+                    d='M1728 647q0 22-26 48l-363 354 86 500q1 7 1 20 0 21-10.5 35.5t-30.5 14.5q-19 0-40-12l-449-236-449 236q-22 12-40 12-21 0-31.5-14.5t-10.5-35.5q0-6 2-20l86-500-364-354q-25-27-25-48 0-37 56-46l502-73 225-455q19-41 49-41t49 41l225 455 502 73q56 9 56 46z'
+                  />
+                </svg>
 
-            <img src={gig.imgUrl} alt='Some Logo Design' />
-            <h2>About Gig</h2>
-            <p>{gig.longerDescription} </p>
-          </section>
-
-          <h2>About The Seller</h2>
-          <section className='seller-avatar flex row align-center gap'>
-            <img src={gig.owner.imgUrl} alt='' />
-            {/* <img
-              className='gig-owner-image border-radius'
-              src='https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8cHJvZmlsZXxlbnwwfHwwfHw%3D&w=1000&q=80'
-            /> */}
-            <div className='flex column'>
-              <Link to={'/#'}> {gig.owner.fullname}</Link>
-              <h4 className='seller-level'>{gig.owner.level}</h4>
-              <h4>{gig.owner.rate}</h4>
-            </div>
-          </section>
-
-          <section className='seller-desc flex column gap'>
-            <div className='formal-desc flex row gap'>
-              <div className='flex column gap'>
-                <h4 className='flex column gap'>
-                  {' '}
-                  <span> From</span> {gig.owner.ownerCountry}
-                </h4>
-                <h4 className='flex column gap'>
-                  {' '}
-                  <span> Member Since</span> {gig.owner.memberSince}
-                </h4>
               </div>
-              <div className='flex column gap'>
-                <h4 className='flex column gap'>
-                  {' '}
-                  <span> Avg. response time</span> {gig.owner.avgResponseTime}
-                </h4>
-                <h4 className='flex column gap'>
-                  {' '}
-                  <span> Last delivery</span> {gig.owner.lastDelivery}
-                </h4>
-              </div>
-            </div>
 
-            <div className='seller-letter'>
-              <p>{gig.owner.ownerLetter}</p>
-            </div>
-          </section>
-          <Link to={`/gig/edit/${gig._id}`}>Edit</Link>
-          <button onClick={() => onRemoveGig(gig._id)}>Delete</button>
-        </div>
-        <aside className='aside'>
-          <div className='package-content'>
-            <h1>Package details</h1>
-            <h3>{gig.name}</h3>
-            <span>{gig.price}$</span>
-            <span className='days-to-delivery'>4 Days Delivery</span>
-            {/* <Link></Link> */}
+              <img src={gig.imgUrl} alt='Some Logo Design' />
+              <h2>About Gig</h2>
+              <p>{gig.longerDescription} </p>
+            </section>
+
+            <h2>About The Seller</h2>
+            <section className='seller-avatar flex row align-center gap'>
+              <img className='gig-owner-image-about border-radius' src={gig.owner.imgUrl} alt='' />
+
+              <div className='flex column'>
+                <Link to={'/#'}> {gig.owner.fullname}</Link>
+                <h4 className='seller-level'>{gig.owner.level}</h4>
+                <div className='flex row'>
+                  <h4>{gig.owner.rate}</h4>
+                  <svg
+                    className='gig-review-star'
+                    xmlns='http://www.w3.org/2000/svg'
+                    viewBox='0 0 1792 1792'
+                    width='16'
+                    height='16'
+                  >
+                    <path
+                      fill='#ffbe5b'
+                      d='M1728 647q0 22-26 48l-363 354 86 500q1 7 1 20 0 21-10.5 35.5t-30.5 14.5q-19 0-40-12l-449-236-449 236q-22 12-40 12-21 0-31.5-14.5t-10.5-35.5q0-6 2-20l86-500-364-354q-25-27-25-48 0-37 56-46l502-73 225-455q19-41 49-41t49 41l225 455 502 73q56 9 56 46z'
+                    />
+                  </svg>
+                </div>
+              </div>
+            </section>
+
+            <section className='seller-desc flex column gap'>
+              <div className='formal-desc flex row gap'>
+                <div className='flex column gap'>
+                  <h4 className='flex column gap'>
+                    {' '}
+                    <span> From</span> {gig.owner.ownerCountry}
+                  </h4>
+                  <h4 className='flex column gap'>
+                    {' '}
+                    <span> Member Since</span> {gig.owner.memberSince}
+                  </h4>
+                </div>
+                <div className='flex column gap'>
+                  <h4 className='flex column gap'>
+                    {' '}
+                    <span> Avg. response time</span> {gig.owner.avgResponseTime}
+                  </h4>
+                  <h4 className='flex column gap'>
+                    {' '}
+                    <span> Last delivery</span> {gig.owner.lastDelivery}
+                  </h4>
+                </div>
+              </div>
+
+              <div className='seller-letter'>
+                <p>{gig.owner.ownerLetter}</p>
+              </div>
+            </section>
+            <Link to={`/gig/edit/${gig._id}`}>Edit</Link>
+            <button onClick={() => onRemoveGig(gig._id)}>Delete</button>
           </div>
-        </aside>
+
+          <aside className='aside flex column gap'>
+            <div className='package-content flex column gap'>
+              <div className='flex row gap' >
+                <h1>Package details</h1>
+                <span>{gig.price}$</span>
+              </div>
+              <span className='days-to-delivery'>{gig.daysToMake} Days To Make </span>
+              <Link to={'/gig/gig-check-out-summery'} className='flex row gap'>Continue <span>(${gig.price})</span></Link>
+            </div>
+          </aside>
+
+        </div>
       </section>
     )
 }
