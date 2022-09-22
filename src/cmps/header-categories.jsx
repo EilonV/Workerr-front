d1c78115d4fcb4172f8d813c93cb471d411c39b3
EilonV <73593619+EilonV@users.noreply.github.com>
2022-09-22 19:46:48 +0300

    fixed header categories style

--- src/cmps/header-categories.jsx
+++ src/cmps/header-categories.jsx
@@ -5,12 +5,24 @@ export function HeaderCategories({ gigs }) {
     <section className='header-categories-wrapper flex'>
       <div className='header-categories flex space-between main-layout'>
         <div className='tags flex space-between'>
-          <a href=''>Graphics & Design</a>
-          <a href=''>Digital Marketing</a>
-          <a href=''>Writing & Translation</a>
-          <a href=''>Video & Animation</a>
-          <a href=''>Music & Audio</a>
-          <a href=''>Programming & Tech</a>
+          <div className='category-section flex align-center'>
+            <a href=''>Graphics & Design</a>
+          </div>
+          <div className='category-section flex align-center'>
+            <a href=''>Digital Marketing</a>
+          </div>
+          <div className='category-section flex align-center'>
+            <a href=''>Writing & Translation</a>
+          </div>
+          <div className='category-section flex align-center'>
+            <a href=''>Video & Animation</a>
+          </div>
+          <div className='category-section flex align-center'>
+            <a href=''>Music & Audio</a>
+          </div>
+          <div className='category-section flex align-center'>
+            <a href=''>Programming & Tech</a>
+          </div>
         </div>
 
       </div>
