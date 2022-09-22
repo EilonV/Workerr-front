01b08feb91dd256b8bcbfd9546da3a245f753aa1
EilonV <73593619+EilonV@users.noreply.github.com>
2022-09-22 19:05:33 +0300

    fixing layout home and header

--- src/cmps/app-header-scroll.jsx
+++ src/cmps/app-header-scroll.jsx
@@ -4,17 +4,20 @@ export function AppHeaderScroll() {
   return (
     <div className='header-scroll'>
       <header className='app-header-scroll main-layout'>
-        <h1 className='logo-scroll header-logo'>
-          Workerr<span>.</span>
-        </h1>
-        <nav className='header-nav'>
-          <NavLink to='/'>Home </NavLink>
-          <NavLink to='/gigs'>Explore </NavLink>
-          <NavLink to='/about'>About </NavLink>
-          <NavLink to='/user'>User </NavLink>
-          <NavLink to='/admin'>Admin </NavLink>
-          <button className='header-join'><NavLink to='/user'>Join </NavLink></button>
-        </nav>
+        <div className='flex align-center space-between'>
+          <h1 className='logo-scroll header-logo'>
+            Workerr<span>.</span>
+          </h1>
+          <nav className='header-nav'>
+            <NavLink to='/'>Home </NavLink>
+            <NavLink to='/gigs'>Explore </NavLink>
+            <NavLink to='/about'>About </NavLink>
+            <NavLink to='/user'>User </NavLink>
+            <NavLink to='/admin'>Admin </NavLink>
+            <button className='header-join'><NavLink to='/user'>Join </NavLink></button>
+          </nav>
+        </div>
+
       </header>
     </div>
 
