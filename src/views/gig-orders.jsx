bb9c7a6b4fdb7ffe72017d7c953accdd9a14352f
Ilya Griss <ilyaig8@gmail.com>
2022-09-23 21:45:36 +0300

    user-nav+gig orders+user page

--- src/views/gig-orders.jsx
+++ src/views/gig-orders.jsx
@@ -1,12 +1,12 @@
 import { useEffect, useState } from 'react'
 import { useSelector, useDispatch } from 'react-redux'
-import { useNavigate, useParams } from 'react-router-dom'
+import { useNavigate, useParams, Link } from 'react-router-dom'
 import { AppHeaderExplore } from '../cmps/app-header-explore'
 import { HeaderCategories } from '../cmps/header-categories'
+import { UserNav } from '../cmps/user-nav'
 import { loadOrders, removeOrder } from '../store/actions/order.action'
-import StarFill from '../assets/imgs/icons/5-stars.svg'
-import Location from '../assets/imgs/icons/location.svg'
-import User from '../assets/imgs/icons/user.svg'
+
+// import { useNavigate, useParams } from 'react-router-dom'
 
 export const Orders = () => {
   // const params = useParams()
@@ -41,80 +41,54 @@ export const Orders = () => {
   }
 
   if (!orders) return ''
-  console.log('orders:', orders)
+  // console.log('orders:', orders)
 
   return (
     <section>
       <AppHeaderExplore />
       <HeaderCategories orders={orders} />
-      <div className='features'>
-        <div className='profile-user flex'>
-          <div className='user-container'>
-            <div className='profile-pic'>U</div>
-            <div className='user-name show'>user</div>
-
-            <div className='user-details'>
-              <div className='from-since flex column'>
-                <img className='star-fill' src={StarFill} alt='star-fill' />
-
-                <div className=''>
-                  <div className='flex '>
-                    <img className='location' src={Location} alt='location' />
-                    <p className='from space-between'>From</p>
-
-                    <div className='flex'>
-                      <p className='country'>Israel</p>
-                    </div>
-                  </div>
-                  <div className=''>
-                    <img className='user' src={User} alt='user' />
-                    <div className='member flex column'></div>
-
-                    <p className='member-since'>Member since</p>
-
-                    <p className='year'>2021</p>
-                  </div>
-                </div>
+      <div className='features '>
+        <div className='profile-user '>
+          <div className='order-container flex'>
+            <UserNav />
+            <section className='total-orders'>
+              <p className='num-orders'>
+                {orders.length === 1
+                  ? orders.length + ' order'
+                  : orders.length + ' orders'}
+              </p>
+              <div className='table-orders'>
+                {orders.map((order) => (
+                  <table key={order._id}>
+                    <tr>
+                      <th> Date </th>
+                      <th> Buyer </th>
+                      <th> Gig</th>
+                      <th> Seller name </th>
+                      <th> Price</th>
+                      <th> Status </th>
+                      <th> Actions </th>
+                    </tr>
+                    <td>{format(order.createdAt)} </td>
+                    <td>{order.buyer.fullname} </td>
+                    <td>{order.gig._id} </td>
+                    <td>{order.seller.fullname} </td>
+                    <td>${order.gig.price} </td>
+                    <td className='status'>{order.status} </td>
+                    <td>
+                      <button
+                        className='delete-btn'
+                        onClick={() => onRemoveOrder(order._id)}
+                      >
+                        Delete
+                      </button>
+                      {/* <button>Update</button> */}
+                    </td>
+                  </table>
+                ))}
               </div>
-            </div>
+            </section>
           </div>
-          <section>
-            <div className='total-orders'>
-              {orders.length === 1
-                ? orders.length + ' order'
-                : orders.length + ' orders'}
-            </div>
-            <div className='table-orders'>
-              {orders.map((order) => (
-                <table key={order._id}>
-                  <tr>
-                    <th> Date </th>
-                    <th> Buyer </th>
-                    <th> Gig</th>
-                    <th> Seller name </th>
-                    <th> Price</th>
-                    <th> Status </th>
-                    <th> Actions </th>
-                  </tr>
-                  <td>{format(order.createdAt)} </td>
-                  <td>{order.buyer.fullname} </td>
-                  <td>{order.gig._id} </td>
-                  <td>{order.seller.fullname} </td>
-                  <td>${order.gig.price} </td>
-                  <td className='status'>{order.status} </td>
-                  <td>
-                    <button
-                      className='delete-btn'
-                      onClick={() => onRemoveOrder(order._id)}
-                    >
-                      Delete
-                    </button>
-                    {/* <button>Update</button> */}
-                  </td>
-                </table>
-              ))}
-            </div>
-          </section>
         </div>
       </div>
     </section>
