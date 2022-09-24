b34f4e48ef15c0fe524aa8d06f328d64d045d2ac
Ilya Griss <ilyaig8@gmail.com>
2022-09-24 21:46:26 +0300

    new commit- alert,new-gig, checkout,orders,userpage

--- src/views/add-new-gig.jsx
+++ src/views/add-new-gig.jsx
@@ -1,38 +1,230 @@
+import { useEffect } from 'react'
+import { useRef } from 'react'
+import { useDispatch } from 'react-redux'
+import { useNavigate, useParams } from 'react-router-dom'
+import { useForm } from '../hooks/useForm'
+import { gigService } from '../services/gig.service'
+import { addGig, updateGig } from '../store/actions/gig.action'
+import { utilService } from '../services/util.service'
 import { AppHeaderExplore } from '../cmps/app-header-explore'
-import ArrowRight from '../assets/imgs/icons/arrow-right.svg'
+import { HeaderCategories } from '../cmps/header-categories'
 
 export const AddNewGig = () => {
+  // const params = useParams()
+  // const navigate = useNavigate()
+  // const dispatch = useDispatch()
+
+  // const [gig, handleChange, setGig] = useForm({
+  //   title: 'I will',
+  //   price: '',
+  //   daysToMake: '',
+  //   longerDescription: '',
+  //   tags: '',
+  //   order: '',
+  //   imgUrl: utilService.makeImg(),
+  //   tags: ['logo - design', 'artisitic', 'proffesional', 'accessible'],
+  //   owner: {
+  //     _id: 'u101',
+  //     fullname: 'Dudu Da',
+  //     imgUrl: 'https://i1.sndcdn.com/artworks-000118768405-0t6s1f-t500x500.jpg',
+  //     level: 'premium',
+  //     rate: 0,
+  //     memberSince: 'march 2015',
+  //     avgResponseTime: '1 hour',
+  //     lastDelivery: 'about 17 hours',
+  //     ownerLetter:
+  //       ' Hi, Mayur here I am a professional graphic designer with an experience of 10+ years. Let my field of expertise collaborate with your level of imagination, so together we can create an exceptional brand image. Something which creates an impact. Impact which screams for its acknowledgment without you needing to do so. Let us make wonders together in this field of designing. Keep exploring',
+  //   },
+  //   user: [{ reviews: '' }],
+  // })
+
+  // const inputRef = useRef()
+  // console.log(inputRef)
+
+  // useEffect(() => {
+  //   inputRef.current.focus()
+  //   const gigId = params.id
+  //   if (!gigId) return
+  //   gigService
+  //     .getById(gigId)
+  //     .then((gig) => {
+  //       setGig(gig)
+  //     })
+  //     .catch((err) => {
+  //       console.log('err:', err)
+  //     })
+  // }, [])
+
+  // const onSaveGig = (ev) => {
+  //   ev.preventDefault()
+  //   if (gig._id) {
+  //     dispatch(updateGig(gig)).then(() => {
+  //       navigate('/gigs')
+  //     })
+  //   } else {
+  //     dispatch(addGig(gig)).then(() => {
+  //       navigate('/gigs')
+  //     })
+  //   }
+  // }
+
   return (
-    <section>
+    <section className='main-container'>
       <AppHeaderExplore />
-      <div className='top-navbar-container wizard-mode main-layout'>
-        <nav className='navbar-overview'>
-          <ul>
-            <li>
-              <li className='list flex'>
-                <p className='number num'>1</p>
-                <p className='text text1'>Order Details</p>
-                <img className='svg' src={ArrowRight} alt='arrow-right' />
-              </li>
-              <li className='list flex'>
-                <p className='number num'>2</p>
-                <p className='text text2'>Order Details</p>
-                <img className='svg' src={ArrowRight} alt='arrow-right' />
-              </li>
-              <li className='list flex'>
-                <p className='number num'>3</p>
-                <p className='text text3'>Order Details</p>
-                <img className='svg' src={ArrowRight} alt='arrow-right' />
-              </li>
-            </li>
-          </ul>
-        </nav>
-        <div className='save-btn-container'>
-          <button type='button' className='naked-button co-blue text-bold'>
-            Save
-          </button>
+      <HeaderCategories />
+      <section className='content-container flex'>
+        <div className='create-container'>
+          {/* <form onSubmit={onSaveGig}> */}
+          <div className='title-container flex'>
+            <div className='title-nav'>
+              <div className='desc' htmlFor='name'>
+                <h1>Gig Title</h1>
+              </div>
+              <p>
+                As your Gig storefront, your title is the most important place
+                to include keywords that buyers would likely use to search for a
+                service like yours.
+              </p>
+            </div>
+
+            <div className='text-area'>
+              <textarea placeholder="I will do something I'm really good at"></textarea>
+
+              <div class='title-footer flex space-between'>
+                <span class='title-status-msg '></span>
+                <span class='chars-count'>0 / 80 max</span>
+              </div>
+            </div>
+          </div>
+
+          <div className='category-container flex'>
+            <div className='title-nav category'>
+              <div className='desc' htmlFor='name'>
+                <h1>Category</h1>
+              </div>
+              <p>
+                Choose the category and sub-category most suitable for your Gig.
+              </p>
+            </div>
+
+            <div className='selection-bar flex space-between'>
+              <select name='tags' id='tag-select'>
+                <option value='digital-marketing'>Digital Marketing</option>
+                <option value='wWriting-translation'>
+                  Writing & Translation
+                </option>
+                <option value='video-animation'>Video & Animation</option>
+                <option value='music-audio'>Music & Audio</option>
+                <option value='programming-Tech'>Programming & Tech</option>
+                <option value='data'>Data</option>
+                <option value='busines'>Busines</option>
+                <option value='lifeStyle'>LifeStyle</option>
+              </select>
+              <select name='tags' id='tag-select'>
+                {/* <option value='digital-marketing'>Digital Marketing</option>
+                <option value='wWriting-translation'>
+                  Writing & Translation
+                </option>
+                <option value='video-animation'>Video & Animation</option>
+                <option value='music-audio'>Music & Audio</option>
+                <option value='programming-Tech'>Programming & Tech</option>
+                <option value='data'>Data</option>
+                <option value='busines'>Busines</option>
+                <option value='lifeStyle'>LifeStyle</option> */}
+              </select>
+              <div class='title-footer flex space-between'></div>
+            </div>
+          </div>
+
+          <div className='tag-container flex '>
+            <div className='tag-nav'>
+              <div className='desc'>
+                <h1>Search Tags</h1>
+              </div>
+              <p>
+                Tag your Gig with buzz words that are relevant to the services
+                you offer. Use all 5 tags to get found.
+              </p>
+            </div>
+            <div className='search-tags'>
+              <div className='desc' htmlFor='name'>
+                <h1>Positive keywords</h1>
+                <p>
+                  Enter search terms you feel your buyers will use when looking
+                  for your service.
+                </p>
+                <textarea placeholder=''></textarea>
+                <h6>5 tags maximum. Use letters and numbers only.</h6>
+              </div>
+            </div>
+          </div>
+          <div className='note'>
+            <p>
+              <span>Please note: </span> Some categories require that sellers
+              verify their skills.
+            </p>
+          </div>
+          {/* <input
+            ref={inputRef}
+            value={gig.title}
+            onChange={handleChange}
+            type='text'
+            name='title'
+            id='title'
+          />
+
+          <label>
+            Days To Make
+            <input
+              placeholder='Days To Make'
+              type='number'
+              id='daysToMake'
+              name='daysToMake'
+              className='input-field'
+              required
+            />
+          </label>
+
+          <div className='add-container'>
+            <label htmlFor='price'>Price</label>
+            <input
+              value={gig.price}
+              onChange={handleChange}
+              type='number'
+              name='price'
+              id='price'
+            />
+          </div>
+
+          <div className='add-container'>
+            <label htmlFor='desc'>Description</label>
+            <textarea
+              name='desc'
+              rows='5'
+              cols='60'
+              type='text'
+              placeholder='Enter Gig Desc...'
+              required=''
+              autocomplete='off'
+            ></textarea>
+          </div>
+
+          <div className='add-container'>
+            <label htmlFor='tag'>Add some tags</label>
+            <input
+              placeholder='Add tags..'
+              value={gig.tags}
+              onChange={handleChange}
+              type='text'
+              name='tag'
+              id='tag'
+            />
+          </div> */}
+
+          {/* <button>Add</button> */}
+          {/* </form> */}
         </div>
-      </div>
+      </section>
     </section>
   )
 }
--- src/views/user-page.jsx
+++ src/views/user-page.jsx
@@ -57,47 +57,48 @@ export const UserPage = () => {
                     It seems that you don't have any active Gigs
                   </h3>
                 </label>
-                <div className='img-create'>
-                  <a
-                    href='/users/ilyaig8/manage_gigs/new'
-                    className='st-current'
-                  >
-                    <svg
-                      className='svg'
-                      xmlns='http://www.w3.org/2000/svg'
-                      width='300'
-                      height='352'
-                      viewBox='0 0 300 352'
+                <Link to='/user/:'>
+                  <div className='img-create'>
+                    <a
+                      href='/users/ilyaig8/manage_gigs/new'
+                      className='st-current'
                     >
-                      <title>Create a Gig</title>
-                      <path
-                        className='hover-block'
-                        fill='#F8F7F7'
-                        d='M29 12h242c9.4 0 17 7.6 17 17v153H12V29c0-9.4 7.6-17 17-17z'
-                      ></path>
-                      <path
-                        className='hover-price'
-                        fill='#B1D8BA'
-                        d='M231 288h20c8.3 0 15 6.7 15 15s-6.7 15-15 15h-20c-8.3 0-15-6.7-15-15s6.7-15 15-15z'
-                      ></path>
-                      <path
-                        className='hover-frame'
-                        fill='#B3B2B2'
-                        d='M270 0H30C13.5 0 0 13.5 0 30v292c0 16.5 13.5 30 30 30h240c16.5 0 30-13.5 30-30V30c0-16.5-13.5-30-30-30zm18 322c0 9.9-8.1 18-18 18H30c-9.9 0-18-8.1-18-18V194h276v128zm0-140H12V30c0-9.9 8.1-18 18-18h240c9.9 0 18 8.1 18 18v152z'
-                      ></path>
-                      <path
-                        className='hover-frame'
-                        fill='#B3B2B2'
-                        d='M34.5 224h145c4.1 0 7.5-3.4 7.5-7.5s-3.4-7.5-7.5-7.5h-145c-4.1 0-7.5 3.4-7.5 7.5s3.4 7.5 7.5 7.5zm0 26h95c4.1 0 7.5-3.4 7.5-7.5s-3.4-7.5-7.5-7.5h-95c-4.1 0-7.5 3.4-7.5 7.5s3.4 7.5 7.5 7.5z'
-                      ></path>
-                    </svg>
-                    <Link to='/user/:'>
+                      <svg
+                        className='svg'
+                        xmlns='http://www.w3.org/2000/svg'
+                        width='300'
+                        height='352'
+                        viewBox='0 0 300 352'
+                      >
+                        <title>Create a Gig</title>
+                        <path
+                          className='hover-block'
+                          fill='#b5b6ba'
+                          d='M29 12h242c9.4 0 17 7.6 17 17v153H12V29c0-9.4 7.6-17 17-17z'
+                        ></path>
+                        <path
+                          className='hover-price'
+                          fill='#1dbf73'
+                          d='M231 288h20c8.3 0 15 6.7 15 15s-6.7 15-15 15h-20c-8.3 0-15-6.7-15-15s6.7-15 15-15z'
+                        ></path>
+                        <path
+                          className='hover-frame'
+                          fill='#74767e'
+                          d='M270 0H30C13.5 0 0 13.5 0 30v292c0 16.5 13.5 30 30 30h240c16.5 0 30-13.5 30-30V30c0-16.5-13.5-30-30-30zm18 322c0 9.9-8.1 18-18 18H30c-9.9 0-18-8.1-18-18V194h276v128zm0-140H12V30c0-9.9 8.1-18 18-18h240c9.9 0 18 8.1 18 18v152z'
+                        ></path>
+                        <path
+                          className='hover-frame'
+                          fill='#b5b6ba'
+                          d='M34.5 224h145c4.1 0 7.5-3.4 7.5-7.5s-3.4-7.5-7.5-7.5h-145c-4.1 0-7.5 3.4-7.5 7.5s3.4 7.5 7.5 7.5zm0 26h95c4.1 0 7.5-3.4 7.5-7.5s-3.4-7.5-7.5-7.5h-95c-4.1 0-7.5 3.4-7.5 7.5s3.4 7.5 7.5 7.5z'
+                        ></path>
+                      </svg>
+
                       <span className='create-btn align-center'>
                         Create a New Gig
                       </span>
-                    </Link>
-                  </a>
-                </div>
+                    </a>
+                  </div>
+                </Link>
               </div>
             </div>
           </div>
